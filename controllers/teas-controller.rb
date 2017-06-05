221c0e342cf8c05f17c2097c15bacf011c754b5d
Ryan Chapman <rchapman@spartaglobal.co>
2017-06-05 16:29:52 +0100

    index added and linked

--- controllers/teas-controller.rb
+++ controllers/teas-controller.rb
@@ -1,17 +1,47 @@
 class TeasController < Sinatra::Base
 
 	# sets root as the parent-directory of the current file	
-	set :root, File.join(File.dirname(__FILE__), '..')
+  set :root, File.join(File.dirname(__FILE__), '..')
 
  	# sets the view directory correctly
-  	set :views, Proc.new { File.join(root, "views") } 
+  set :views, Proc.new { File.join(root, "views") } 
 
-  	configure :development do
-    	register Sinatra::Reloader
-  	end
+	configure :development do
+  	register Sinatra::Reloader
+	end
 
-  	#index
-  	get '/' do
-  		"hello"
-  	end
+	#index
+	get '/' do
+		@title = "Tea life"
+
+    erb :"teas/index"
+	end
+
+	get '/new' do
+		erb :"teas/new"
+	end
+
+	get '/:id' do
+		id = params[:id].to_i
+		"SHOW: #{id}"
+  end
+
+	post '/' do
+		"posted"
+	end
+
+	put '/:id' do
+	end
+
+	delete '/:id' do
+		id = params[:id].to_i
+		"delete"
+	end
+
+	get '/:id/edit' do
+		id = params[:id].to_i
+		"edit: #{id}"
+	end
+
+
 end
